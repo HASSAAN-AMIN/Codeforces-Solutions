5a4fc3eaa58733f0b14a15f8d8bf09c0e527cc13
HASSAAN-AMIN <hassaanamin4133@gmail.com>
2026-03-13 04:09:11 +0500

    dyk we left each other not bcz we there was smth wrong but bcz we love each other so much?

---
+++ 1476A.cpp
@@ -0,0 +1,25 @@
+/* 
+                بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ   
+    In the name of Allah, the Most Gracious, the Most Merciful.
+*/
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while(t--){
+        int n, k;
+        cin >> n >> k;
+
+        long long s = ((n + k - 1) / k) * k;
+        cout << (s + n - 1) / n << '\n';
+    }
+
+    return 0;
+}
