306c01875487205c045815a995df5f4346f56637
HASSAAN-AMIN <hassaanamin4133@gmail.com>
2026-03-19 03:29:07 +0500

    hello world

--- 2208A.cpp
+++ 2208A.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include<vector>
 #include<algorithm>
+#include <map>
 
 using namespace std;
 
@@ -17,15 +18,29 @@ int main() {
     while(t--){
         int n;
         cin >> n;
-        vector<vector<int> > arr( n  ,  vector<int>(n) ) ;  
-        vector<int> ans( n*n +1  ,  0 ) ; 
         bool sus = false ;
+        // smarter version below 
+        // vector<vector<int> > arr( n  ,  vector<int>(n) ) ;  
+        // vector<int> ans( n*n +1  ,  0 ) ; 
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = 0; j < n; j++) {
+        //         cin >> arr[i][j] ;
+        //         ans[arr[i][j]]++;
+        //         if(!sus && ans[arr[i][j]] > (n*(n-1)))
+        //             sus = true;
+        //     }
+        // }
+        // smarter version below 
+        int x ;
+        map <int , int > mapy ;
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                cin >> arr[i][j] ;
-                ans[arr[i][j]]++;
-                if(!sus && ans[arr[i][j]] >= (2*n-1))
+                cin >> x ; 
+                mapy[x]++;
+                if(!sus && mapy[x] > (n*n-n)){
                     sus = true;
+                }
             }
         }
         if(sus)
