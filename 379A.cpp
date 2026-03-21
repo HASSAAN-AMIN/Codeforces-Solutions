b4c8a21968224a490cc36ea11b73916756f74bb1
HASSAAN-AMIN <hassaanamin4133@gmail.com>
2026-03-22 01:37:26 +0500

    uh uh uhhh

---
+++ 379A.cpp
@@ -0,0 +1,32 @@
+/* 
+                بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ   
+    In the name of Allah, the Most Gracious, the Most Merciful.
+*/
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int a , b ;
+    cin >> a >> b ;
+    int t = b;
+
+    int ans = a; 
+    int crnt = a;  
+
+    while (crnt >= b) {
+        ans += crnt / b;
+        crnt = (crnt % b) + crnt / b;
+    }
+
+    cout << ans << '\n';
+
+    return 0;
+}
+
+
+
+ 
