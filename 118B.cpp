0306ac2c77a0e8ec01efb1a37c8be02dd3ce8cc0
HASSAAN-AMIN <hassaanamin4133@gmail.com>
2026-03-23 05:48:56 +0500

    should i sleep now?

---
+++ 118B.cpp
@@ -0,0 +1,39 @@
+/* 
+                بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ   
+    In the name of Allah, the Most Gracious, the Most Merciful.
+*/
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n ;
+    cin >> n ;
+
+
+    //       0
+    //     0 1 0
+    //   0 1 2 1 0
+    // 0 1 2 3 2 1 0
+    //   0 1 2 1 0
+    //     0 1 0
+    //       0
+
+    for (int i = -n; i <= n; i++) {    
+        for (int j = -n; j <= n; j++) {
+            if (n - (abs(i) + abs(j)) >= 0) {
+                cout << n - (abs(i) + abs(j)) << ' ';
+            } else {
+                cout << "  "; 
+            }
+        }
+        cout << '\n';
+    }
+
+
+
+    return 0;
+}
