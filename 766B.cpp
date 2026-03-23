bf1b51eb327d0ca93d84118f8b4244e1d9533bb8
HASSAAN-AMIN <hassaanamin4133@gmail.com>
2026-03-23 05:04:45 +0500

    triangles

---
+++ 766B.cpp
@@ -0,0 +1,37 @@
+/* 
+                بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ   
+    In the name of Allah, the Most Gracious, the Most Merciful.
+*/
+#include <iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n ;
+    cin >> n ;
+    vector<int> arr(n) ;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i] ;
+    }
+    sort(arr.begin() , arr.end());
+
+    // auto check = [](int i, int j, int k) {
+    //     return (i + j  > k) || (i + k >  j) || (j + k > i);
+    // };
+
+    for (int i = 0; i < n-2; i++) {
+        if(arr[i] + arr[i+1] > arr[i+2]){
+            cout << "YES" << '\n' ;
+            return 0;
+        }
+    }
+    cout << "NO" << '\n' ;
+
+
+    return 0;
+}
