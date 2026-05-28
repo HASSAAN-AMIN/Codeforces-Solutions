e8c54bfab415ddaece1bbdf7a2b8a7d0e7fe8a25
Hassaan Amin <hassaanamin4133@gmail.com>
2026-05-29 00:24:08 +0500

    tis problem was second hardest thing in my room

---
+++ 2231C.cpp
@@ -0,0 +1,54 @@
+/* 
+                بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ   
+    In the name of Allah, the Most Gracious, the Most Merciful.
+*/
+#include <iostream>
+#include<vector>
+#include<algorithm>
+#include <map>
+#include <set>
+
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        vector<int> arr(n) ;
+        for (int i = 0; i < n; i++) 
+            cin >> arr[i] ;
+        map< int , pair<int, int> > mapy ; 
+        for (int i = 0; i < n; i++) {
+            int x = arr[i] ; 
+            set<int> vis ; 
+            int ops = 0 ;
+            while(!vis.count(x)){
+                vis.insert(x) ;
+                if( i == 0 || mapy.count(x)){
+                    mapy[x].first += ops ; 
+                    mapy[x].second += 1 ;
+                }
+                ops++;
+                if(x%2)
+                    x++;
+                else 
+                    x /= 2; 
+            }
+        }
+        int ans  = 2e9 ;
+
+        for( auto it : mapy)
+            if(it.second.second == n)
+                ans = min( ans  , it.second.first) ;
+        cout << ans << '\n' ;
+
+    }
+
+    return 0;
+}
