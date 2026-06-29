83fdecb43180c0d049e501130f326c9187904fa8
HASSAAN-AMIN <hassaanamin4133@gmail.com>
2026-06-29 22:56:56 +0500

    i am genius

--- 295A.cpp
+++ 295A.cpp
@@ -17,17 +17,52 @@ int main() {
     // while(t--){
     //     int n;
     //     cin >> n;
-        
     // }
+
+
     int n, m , k;
     cin >> n >> m >> k ;
 
-    vector<int> arr(n) ;
-    for (int i = 0; i < n; i++) {
+    vector<long long > arr(n +1  ,0 ) ;
+    for (int i = 1; i < n+1; i++) {
         cin >> arr[i];
     }
-    
 
+    vector<long long > dif( n+2 , 0 ) ; 
+
+    vector< vector<int> > ops( m , vector<int>(3) )  ;
+    
+    for (int i = 0; i < m; i++) {
+        cin >> ops[i][0] >> ops[i][1] >> ops[i][2] ; 
+    }
+
+    vector<long long> times( m+ 2); 
+    int x , y ;
+    for (int i = 0; i < k; i++) {
+        cin >> x >> y ;
+        times[x]++;
+        times[y+1]--;
+    }
+
+    for (int i = 1; i < m +1 ; i++) {
+        times[i] += times[i-1];
+
+        dif[ ops[i-1][0]  ] += times[i]*ops[i-1][2];
+        dif[ ops[i-1][1]  + 1 ] -= times[i]*ops[i-1][2];
+
+    }
+
+
+    for (int i = 1; i < n+2; i++) {
+        dif[i] += dif[i-1];
+    }
+
+
+    for (int i = 1; i < n+1; i++) {
+        cout << arr[i] + dif[i] << ' ' ;
+    }
+    // omg i am genius
+    cout << '\n';
 
     return 0;
 } 
